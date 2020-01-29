608054aea4509bee397f0d54cf515152616f40c3
Quang Nguyen <quangnguyen@Quangs-MacBook-Pro.local>
2020-01-29 13:30:08 -0800

    finished hello, world! app

--- Hello, World!/Hello, World!/ContentView.swift
+++ Hello, World!/Hello, World!/ContentView.swift
@@ -9,13 +9,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, World!")
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        NavigationView {
+            List {
+                Text("Hello, World!")
+                    .font(.largeTitle)
+                Text("Hello, World!")
+                    .font(.title)
+                Text("Hello, World!")
+                    .font(.headline)
+                Text("Hello, World!")
+                    .font(.body)
+                Text("Hello, World!")
+                    .font(.callout)
+                Text("Hello, World!")
+                    .font(.subheadline)
+                Text("Hello, World!")
+                    .font(.footnote)
+                Text("Hello, World!")
+                    .font(.caption)
+                Text("Hello, World!")
+                    .font(nil)
+            }.navigationBarTitle(Text("Hello, World!"))
+        }
+        
     }
 }
--- Hello, World!/Hello, World!/SceneDelegate.swift
+++ Hello, World!/Hello, World!/SceneDelegate.swift
@@ -18,6 +18,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+            
+        UINavigationBar.appearance().barTintColor = .systemBackground
+        UINavigationBar.appearance().tintColor = .label
+        UINavigationBar.appearance().shadowImage = UIImage()
+        UITableView.appearance().separatorStyle = .none
 
         // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView()
